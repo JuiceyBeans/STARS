e4984c018b61ac98316a0bb1f5c51f0050b9e81d
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2025-01-09 15:21:47 +0000

    Task 1 (#1)
    * Add getters and setters for Permit
    
    * Oops remove test field
    
    * Add a constructor lmao
    
    * Throw exception instead of System.out
    
    * Oops add pointCount
    
    * Fix types
    
    * Token methods + toString()
    
    * Shuttle entry handler
    
    * Add bool to check if enough tokens for shuttle
    
    * Update version
    
    * Update .gitignore
    
    * Fix point -> token conversions
    
    * Exception for rating if out of range
    
    * Method overload for hasTokensForShuttle()
    
    * PermitTester
    
    * Add testing for shuttleEntry()
    
    * Make fields private access oops

--- Permit.java
+++ Permit.java
@@ -1,14 +1,111 @@
- 
 /**
- * A Permit has an id number, name, a luxury rating,  tokens and points.
+ * A Permit has an id number, name, a luxury rating, tokens and points.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Karthi (Ayush) Suresh
+ * @version 9/1/2025
  */
-public class Permit 
-{
-    
-   
-    
-}
+public class Permit {
+    private int id;
+    private String guestName;
+    private int rating;
+    private int tokenCount;
+    private int pointCount;
 
+    // MAN IT SURE WOULD BE NICE TO BE ABLE TO AUTOGEN GETTERS AND SETTERS
+    // Update: You CAN actually do this with IntelliJ, hell yeah
+
+    public Permit(int id, String guestName, int rating, int tokenCount) {
+        this.id = id;
+        this.guestName = guestName;
+        if (rating < 1 || rating > 10) {
+            throw new IllegalArgumentException("Rating needs to be between 1 and 10");
+        }
+        this.rating = rating;
+        this.tokenCount = tokenCount;
+        this.pointCount = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return guestName;
+    }
+    public void setName(String name) {
+        this.guestName = name;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        if (rating <= 10 && rating >= 1) {
+            this.rating = rating;
+        } else {
+            throw new IllegalArgumentException("Rating needs to be between 1 and 10");
+        }
+    }
+    public int getTokenCount() {
+        return tokenCount;
+    }
+    public void setTokenCount(int tokenCount) {
+        this.tokenCount = tokenCount;
+    }
+    public int getPointCount() {
+        return pointCount;
+    }
+    public void setPointCount(int pointCount) {
+        this.pointCount = pointCount;
+    }
+
+    public void addTokens(int tokens) {
+        this.tokenCount = this.tokenCount + tokens;
+    }
+    public void removeTokens(int tokens) {
+        this.tokenCount = this.tokenCount - tokens;
+    }
+
+    /**
+     * @return How many tokens can be obtained by converting points
+     */
+    public int returnPointsToTokens() {
+        return this.pointCount / 4;
+    }
+    public void convertPointsToTokens() {
+        while (pointCount >= 4) {
+            tokenCount += 1;
+            pointCount -= 4;
+        }
+    }
+
+    public boolean hasTokensForShuttle(int shuttleCost) {
+        int tokenCount = this.getTokenCount();
+        return tokenCount >= shuttleCost;
+    }
+    public boolean hasTokensForShuttle() {
+        return hasTokensForShuttle(3);
+    }
+
+    public void shuttleEntry() { // a method to make changes to permit details when a shuttle is entered
+        int shuttleCost = 3;
+        if (hasTokensForShuttle(shuttleCost)) {
+            removeTokens(shuttleCost);
+            pointCount += 1;
+        } else {
+            throw new IllegalArgumentException("Not enough tokens to use the shuttle");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Permit{" +
+                "id=" + id +
+                ", guestName='" + guestName + '\'' +
+                ", rating=" + rating +
+                ", tokenCount=" + tokenCount +
+                ", pointCount=" + pointCount +
+                '}';
+    }
+}
--- PermitTester.java
+++ PermitTester.java
@@ -2,16 +2,53 @@
 /**
  * Write a description of class PermitTester here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Karthi (Ayush) Suresh
+ * @version 9/1/2025
  */
-public class PermitTester
-{
-    public static void main(String[] args)
-    {
-        
-        
+public class PermitTester {
+    public static void permitTests() {
+        Permit testPermit1 = new Permit(1, "Magnus Arkyve", 6, 9);
+        Permit testPermit2 = new Permit(2, "Frederick Fabear", 3, 1);
+        Permit testPermit3 = new Permit(3, "Hatsune Miku", 9, 1);
+
+        System.out.println(testPermit1);
+        System.out.println("Name: " + testPermit1.getName());
+        testPermit1.setPointCount(19);
+        System.out.println("Tokens before conversion: " + testPermit1.getTokenCount());
+        System.out.println("Points before conversion: " + testPermit1.getPointCount());
+        System.out.println("Tokens obtainable from converting points: " + testPermit1.returnPointsToTokens());
+        testPermit1.convertPointsToTokens();
+        System.out.println("Tokens post conversion: " + testPermit1.getTokenCount());
+        System.out.println("Points after conversion: " + testPermit1.getPointCount());
+
+
+        System.out.println(testPermit2);
+        System.out.println("Token count: " + testPermit2.getTokenCount());
+        System.out.println("Does permit have enough tokens to use shuttle: " + testPermit2.hasTokensForShuttle());
+        testPermit2.addTokens(6);
+        System.out.println("Token count: " + testPermit2.getTokenCount());
+        System.out.println("Does permit have enough tokens to use shuttle: " + testPermit2.hasTokensForShuttle());
+        testPermit2.removeTokens(3);
+        System.out.println("Token count: " + testPermit2.getTokenCount());
+
+
+        System.out.println(testPermit3);
+        System.out.println("Rating: " + testPermit3.getRating());
+        testPermit3.setRating(1);
+        System.out.println("Rating: " + testPermit3.getRating());
+        // Exception testing
+        // testPermit3.setRating(13);
+        // testPermit3.shuttleEntry();
+        System.out.println("Token count: " + testPermit3.getTokenCount());
+        System.out.println("Point count: " + testPermit3.getPointCount());
+        testPermit3.addTokens(3);
+        System.out.println("Token count: " + testPermit3.getTokenCount());
+        testPermit3.shuttleEntry();
+        System.out.println("Token count: " + testPermit3.getTokenCount());
+        System.out.println("Point count: " + testPermit3.getPointCount());
     }
-        
-    
+
+    public static void main(String[] args) {
+        permitTests();
+    }
 }
